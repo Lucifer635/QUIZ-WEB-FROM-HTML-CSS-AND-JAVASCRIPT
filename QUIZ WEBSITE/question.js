72c3a8afe7d7fdea60066b23fd4d15ff7d08b063
Lucifer635 <146217584+Lucifer635@users.noreply.github.com>
2024-10-22 08:44:18 +0800

    Add files via upload

---
+++ QUIZ WEBSITE/question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Apa tag HTML yang digunakan untuk membuat paragraf?",
+        answer: "A. p",
+        options: [
+            "A. p",
+            "B. paragraph",
+            "C. text",
+            "D. img"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Apa properti CSS yang digunakan untuk mengubah ukuran font?",
+        answer: "A. font-size",
+        options: [
+            "A. font-size",
+            "B. text-size",
+            "C. size",
+            "D. font-style"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Apa kepanjangan dari HTML?",
+        answer: "A. Hypertext Markup Language",
+        options: [
+            "A. Hypertext Markup Language",
+            "B. Hightext Markup Language",
+            "C. Hyperlink and Text Markup Language",
+            "D. Hypertext Multi Language"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Apa tag yang digunakan untuk membuat judul utama dalam HTML?",
+        answer: "A. h1",
+        options: [
+            "A. h1",
+            "B. title",
+            "C. head",
+            "D. header"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Apa nilai dari properti color yang digunakan untuk mengubah teks menjadi merah?",
+        answer: "D. Semua jawaban benar",
+        options: [
+            "A. red",
+            "B. #ff0000",
+            "C. rgb(255, 0, 0)",
+            "D. Semua jawaban benar"
+        ]
+    }
+];
